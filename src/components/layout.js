52c42adea040f2bbd273eebbfb028348e0c9f7b3
Elicia Au Duong <eliciaauduong@outlook.com>
2020-09-12 20:42:22 +1000

    added navbar and footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,27 @@
 import React from "react"
-import Header from "../components/header"
+import { Link } from "gatsby"
+import "../styles/navbar.css"
+
+const ListLink = props => (
+  <li style={{ display: `inline-block` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
 
 export default function Layout({ children }) {
   return (
-    <div style={{ margin: `90px auto`, width: 1000, padding: `0` }}>
-      <Header>{children}</Header>
+    <div>
+      <header>
+        <ul style={{ listStyle: `none`, textAlign: `center` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/blog">Blog</ListLink>
+          <ListLink to="/resources">Resources</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
+      </header>
+      <main>{children}</main>
+      <footer>Copyright © 2020. Elicia Au Duong</footer>
     </div>
   )
 }
