0e1238021e6de269219703541dd46a7f2a498216
Elicia Au Duong <eliciaauduong@outlook.com>
2020-09-16 23:40:13 +1000

    added social media links to contact

---
+++ src/pages/contact.js
@@ -0,0 +1,60 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Contact</h1>
+      <div>
+        <h2>Stay connected!</h2>
+        <ul>
+          <li>
+            <a
+              href="https://twitter.com/eliciaauduong"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/eliciaauduong/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.pinterest.com.au/eliciaauduong"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Pinterest
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/eliciaauduong/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/eliciaauduong"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
+    </Layout>
+  )
+}
