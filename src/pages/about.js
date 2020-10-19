95f769b5ed5f2ca03933406fa79265c28fc00e0c
Elicia Au Duong <eliciaauduong@outlook.com>
2020-10-19 22:37:20 +1100

    created simple about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,25 +1,28 @@
 import React from "react"
-import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import Enactus from "../../static/enactus.png"
+import Redback from "../../static/redback.png"
 
-export default function About({ data }) {
+export default function About() {
   return (
     <Layout>
-      <h1>About {data.site.siteMetadata.title}</h1>
-      <p>
-        We're the only site running on your computer dedicated to showing the
-        best photos and videos of pandas eating lots of food.
+      <h1>About</h1>
+      <p class="about-desc">
+        Hi! I'm Elicia, a second-year Commerce/ Computer Science student @ UNSW,
+        majoring in International Business and Business Economics.
+      </p>
+      <p class="about-desc">
+        I'm interested in areas such as design, web development, data analysis
+        and international business.
+      </p>
+      <h2>Teams</h2>
+      <div id="groups">
+        <img src={Enactus} alt="Enactus" class="group-logo" />
+        <img src={Redback} alt="Redback" class="group-logo" />
+      </div>
+      <p class="about-desc">
+        Have any questions? <a href="/contact/">Get in touch</a>!
       </p>
     </Layout>
   )
 }
-
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
