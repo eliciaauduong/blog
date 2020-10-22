111457f37caa67c180fb6253b919fc565d10c04c
Elicia Au Duong <eliciaauduong@outlook.com>
2020-10-22 18:00:24 +1100

    added photo and description to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,77 +10,91 @@ import {
   faDev,
 } from "@fortawesome/free-brands-svg-icons"
 import Layout from "../components/layout"
+import Contact from "../../static/contact.jpg"
 
 export default function Home() {
   return (
     <Layout>
-      <h1 class="heading">Contact</h1>
-      <div id="socials">
-        <h2>
-          Let's stay connected!{" "}
-          <span role="img" aria-label="sparkles">
-            ✨
-          </span>
-        </h2>
-        <div>
-          <a
-            href="https://twitter.com/eliciaauduong"
-            target="_blank"
-            rel="noreferrer"
-            class="social-link"
-          >
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-          <a
-            href="https://www.instagram.com/eliciaauduong/"
-            target="_blank"
-            rel="noreferrer"
-            class="social-link"
-          >
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-          <a
-            href="https://www.pinterest.com.au/eliciaauduong"
-            target="_blank"
-            rel="noreferrer"
-            class="social-link"
-          >
-            <FontAwesomeIcon icon={faPinterest} />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/eliciaauduong/"
-            target="_blank"
-            rel="noreferrer"
-            class="social-link"
-          >
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-          <a
-            href="https://github.com/eliciaauduong"
-            target="_blank"
-            rel="noreferrer"
-            class="social-link"
-          >
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a
-            href="https://codepen.io/eliciaauduong"
-            target="_blank"
-            rel="noreferrer"
-            class="social-link"
-          >
-            <FontAwesomeIcon icon={faCodepen} />
-          </a>
-          <a
-            href="https://dev.to/eliciaauduong"
-            target="_blank"
-            rel="noreferrer"
-            class="social-link"
-          >
-            <FontAwesomeIcon icon={faDev} />
-          </a>
-        </div>
-      </div>
+      <main class="sub-page">
+        <section>
+          <h1>Contact</h1>
+          <img src={Contact} alt="desk layout" id="contact-header" />
+          <div id="contact-info">
+            <p>
+              Thank you for checking out my blog. If you're interested in what I
+              do or have any questions, the best way to connect with me is on{" "}
+              <a
+                href="https://twitter.com/eliciaauduong"
+                target="_blank"
+                rel="noreferrer"
+                class="link"
+              >
+                Twitter
+              </a>
+              !
+            </p>
+          </div>
+          <div id="socials">
+            <h2>Follow me on social media!</h2>
+            <a
+              href="https://twitter.com/eliciaauduong"
+              target="_blank"
+              rel="noreferrer"
+              class="social-link"
+            >
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a
+              href="https://www.instagram.com/eliciaauduong/"
+              target="_blank"
+              rel="noreferrer"
+              class="social-link"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a
+              href="https://www.pinterest.com.au/eliciaauduong"
+              target="_blank"
+              rel="noreferrer"
+              class="social-link"
+            >
+              <FontAwesomeIcon icon={faPinterest} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/eliciaauduong/"
+              target="_blank"
+              rel="noreferrer"
+              class="social-link"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a
+              href="https://github.com/eliciaauduong"
+              target="_blank"
+              rel="noreferrer"
+              class="social-link"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a
+              href="https://codepen.io/eliciaauduong"
+              target="_blank"
+              rel="noreferrer"
+              class="social-link"
+            >
+              <FontAwesomeIcon icon={faCodepen} />
+            </a>
+            <a
+              href="https://dev.to/eliciaauduong"
+              target="_blank"
+              rel="noreferrer"
+              class="social-link"
+            >
+              <FontAwesomeIcon icon={faDev} />
+            </a>
+          </div>
+        </section>
+      </main>
     </Layout>
   )
 }
