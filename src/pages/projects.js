18fb1586f719ea4c04c6bb34ee63c92299cb51c6
Elicia Au Duong <eliciaauduong@outlook.com>
2020-10-22 18:00:49 +1100

    created projects page layout template

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,122 +4,128 @@ import Layout from "../components/layout"
 export default function Home() {
   return (
     <Layout>
-      <h1>Projects</h1>
-      <div id="projects">
-        <div class="project-tile">
-          <h3>
+      <main class="sub-page">
+        <h1>Projects</h1>
+        <section id="projects">
+          <div class="project-tile">
             <a
               href="https://codepen.io/eliciaauduong/full/zYqmeZg"
-              class="project-title"
               target="_blank"
+              rel="noreferrer"
             >
-              <span role="img" aria-label="rocket">
-                🚀
-              </span>{" "}
-              freeCodeCamp: Personal Portfolio
+              <h2>
+                <span role="img" aria-label="rocket">
+                  🚀
+                </span>{" "}
+                freeCodeCamp: Personal Portfolio
+              </h2>
             </a>
-          </h3>
-          <p id="project-desc">
-            <span class="project-date">September 21, 2020</span> Project 5/5 for
-            Responsive Web Design Certification from freeCodeCamp: Personal
-            Portfolio showcasing projects I designed and built
-          </p>
-        </div>
-        <div class="project-tile">
-          <h3>
+            <p class="project-date">September 21, 2020</p>
+            <p id="project-desc">
+              Project 5/5 for Responsive Web Design Certification from
+              freeCodeCamp: Personal Portfolio showcasing projects I designed
+              and built
+            </p>
+          </div>
+          <div class="project-tile">
             <a
               href="https://codepen.io/eliciaauduong/full/KKzxrqz"
-              class="project-title"
               target="_blank"
+              rel="noreferrer"
             >
-              <span role="img" aria-label="snake">
-                🐍
-              </span>{" "}
-              freeCodeCamp: Technical Documentation Page
+              <h2>
+                <span role="img" aria-label="snake">
+                  🐍
+                </span>{" "}
+                freeCodeCamp: Technical Documentation Page
+              </h2>
             </a>
-          </h3>
-          <p id="project-desc">
-            <span class="project-date">September 18, 2020</span> Project 4/5 for
-            Responsive Web Design Certification from freeCodeCamp: Technical
-            Documentation Page for basic Python concepts
-          </p>
-        </div>
-        <div class="project-tile">
-          <h3>
+            <p class="project-date">September 18, 2020</p>
+            <p id="project-desc">
+              Project 4/5 for Responsive Web Design Certification from
+              freeCodeCamp: Technical Documentation Page for basic Python
+              concepts
+            </p>
+          </div>
+          <div class="project-tile">
             <a
               href="https://codepen.io/eliciaauduong/full/QWNMbze"
-              class="project-title"
               target="_blank"
+              rel="noreferrer"
             >
-              <span role="img" aria-label="doughnut">
-                🍩
-              </span>{" "}
-              freeCodeCamp: Product Landing Page
+              <h2>
+                <span role="img" aria-label="doughnut">
+                  🍩
+                </span>{" "}
+                freeCodeCamp: Product Landing Page
+              </h2>
             </a>
-          </h3>
-          <p id="project-desc">
-            <span class="project-date">August 31, 2020</span> Project 3/5 for
-            Responsive Web Design Certification from freeCodeCamp: Product
-            Landing Page for an imaginary donut subscription service
-          </p>
-        </div>
-        <div class="project-tile">
-          <h3>
+            <p class="project-date">August 31, 2020</p>
+            <p id="project-desc">
+              Project 3/5 for Responsive Web Design Certification from
+              freeCodeCamp: Product Landing Page for an imaginary donut
+              subscription service
+            </p>
+          </div>
+          <div class="project-tile">
             <a
               href="https://codepen.io/eliciaauduong/full/poywRpw"
-              class="project-title"
               target="_blank"
+              rel="noreferrer"
             >
-              <span role="img" aria-label="check mark button">
-                ✅
-              </span>{" "}
-              freeCodeCamp: Survey Form
+              <h2>
+                <span role="img" aria-label="check mark button">
+                  ✅
+                </span>{" "}
+                freeCodeCamp: Survey Form
+              </h2>
             </a>
-          </h3>
-          <p id="project-desc">
-            <span class="project-date">August 29, 2020</span> Project 2/5 for
-            Responsive Web Design Certification from freeCodeCamp: A Feedback
-            Survey Form for Baymax (inspired by Big Hero 6)
-          </p>
-        </div>
-        <div class="project-tile">
-          <h3>
+            <p class="project-date">August 29, 2020</p>
+            <p id="project-desc">
+              Project 2/5 for Responsive Web Design Certification from
+              freeCodeCamp: A Feedback Survey Form for Baymax (inspired by Big
+              Hero 6)
+            </p>
+          </div>
+          <div class="project-tile">
             <a
               href="https://codepen.io/eliciaauduong/full/gOrgXGW"
-              class="project-title"
               target="_blank"
+              rel="noreferrer"
             >
-              <span role="img" aria-label="airplane departure">
-                🛫
-              </span>{" "}
-              freeCodeCamp: Tribute Page
+              <h2>
+                <span role="img" aria-label="airplane departure">
+                  🛫
+                </span>{" "}
+                freeCodeCamp: Tribute Page
+              </h2>
             </a>
-          </h3>
-          <p id="project-desc">
-            <span class="project-date">August 27, 2020</span> Project 1/5 for
-            Responsive Web Design Certification from freeCodeCamp: A Tribute
-            Page for Amelia Earhart
-          </p>
-        </div>
-        <div class="project-tile">
-          <h3>
+            <p class="project-date">August 27, 2020</p>
+            <p id="project-desc">
+              Project 1/5 for Responsive Web Design Certification from
+              freeCodeCamp: A Tribute Page for Amelia Earhart
+            </p>
+          </div>
+          <div class="project-tile">
             <a
               href="https://eliciaauduong.github.io/digital-garden/"
-              class="project-title"
               target="_blank"
+              rel="noreferrer"
             >
-              <span role="img" aria-label="seedling">
-                🌱
-              </span>{" "}
-              Digital Garden
+              <h2>
+                <span role="img" aria-label="seedling">
+                  🌱
+                </span>{" "}
+                Digital Garden
+              </h2>
             </a>
-          </h3>
-          <p id="project-desc">
-            <span class="project-date">August 7, 2020</span> An ongoing work in
-            progress of all my thoughts and notes
-          </p>
-        </div>
-      </div>
+            <p class="project-date">August 7, 2020</p>
+            <p id="project-desc">
+              An ongoing work in progress of all my thoughts and notes
+            </p>
+          </div>
+        </section>
+      </main>
     </Layout>
   )
 }
